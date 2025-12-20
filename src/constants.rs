146823778ee2043b194cbb6adff626fed0d9b439
わん <wanko.inuco@gmail.com>
2025-12-20 12:25:47 +0900

    move atlas pic

--- src/constants.rs
+++ src/constants.rs
@@ -6,54 +6,52 @@ pub const BLOCK_SIZE: f32 = 8.0;
 
 // Game Bank (Bank 0) Mappings
 pub const SPRITE_SELECT_NORMAL: Rect = Rect {
-    x: 0.0,
-    y: 0.0,
+    x: 8.0,
+    y: 8.0,
     w: 8.0,
     h: 8.0,
 };
 pub const SPRITE_SELECT_LARGE: Rect = Rect {
-    x: 0.0,
-    y: 8.0,
+    x: 8.0,
+    y: 16.0,
     w: 10.0,
     h: 10.0,
 };
 pub const SPRITE_CURSOR: Rect = Rect {
-    x: 16.0,
+    x: 8.0,
     y: 0.0,
     w: 8.0,
     h: 8.0,
 };
 
-const BANK_1_OFFSET_Y: f32 = 24.0;
-
 pub const SPRITE_BLOCK_DIRT: Rect = Rect {
-    x: 8.0,
-    y: BANK_1_OFFSET_Y + 0.0,
+    x: 48.0,
+    y: 8.0,
     w: 8.0,
     h: 8.0,
 };
 pub const SPRITE_BLOCK_GRASS: Rect = Rect {
-    x: 16.0,
-    y: BANK_1_OFFSET_Y + 0.0,
+    x: 48.0,
+    y: 0.0,
     w: 8.0,
     h: 8.0,
 };
 pub const SPRITE_BLOCK_STONE: Rect = Rect {
-    x: 8.0,
-    y: BANK_1_OFFSET_Y + 8.0,
+    x: 56.0,
+    y: 0.0,
     w: 8.0,
     h: 8.0,
 };
 pub const SPRITE_BLOCK_COAL: Rect = Rect {
-    x: 16.0,
-    y: BANK_1_OFFSET_Y + 8.0,
+    x: 56.0,
+    y: 8.0,
     w: 8.0,
     h: 8.0,
 };
 
 // Animation
-pub const SPRITE_BREAK_ANIM_U: f32 = 0.0;
-pub const SPRITE_BREAK_ANIM_V_START: f32 = BANK_1_OFFSET_Y + 0.0;
+pub const SPRITE_BREAK_ANIM_U: f32 = 40.0;
+pub const SPRITE_BREAK_ANIM_V_START: f32 = 0.0;
 
 use macroquad::color::Color;
 
