fd8463235443645eb58c047fc0cd986e421ef799
わん <wanko.inuco@gmail.com>
2025-12-20 21:11:58 +0900

    texture

--- src/render/sprites.rs
+++ src/render/sprites.rs
@@ -20,6 +20,7 @@ pub const SPRITE_CURSOR: Rect = Rect {
     h: 8.0,
 };
 
+// blocks
 pub const SPRITE_BLOCK_DIRT: Rect = Rect {
     x: 48.0,
     y: 8.0,
@@ -38,11 +39,83 @@ pub const SPRITE_BLOCK_STONE: Rect = Rect {
     w: 8.0,
     h: 8.0,
 };
+
+// ore blocks
 pub const SPRITE_BLOCK_COAL: Rect = Rect {
+    x: 56.0,
+    y: 0.0,
+    w: 8.0,
+    h: 8.0,
+};
+pub const SPRITE_BLOCK_COPPER: Rect = Rect {
     x: 56.0,
     y: 8.0,
     w: 8.0,
     h: 8.0,
+};
+pub const SPRITE_BLOCK_IRON: Rect = Rect {
+    x: 56.0,
+    y: 16.0,
+    w: 8.0,
+    h: 8.0,
+};
+pub const SPRITE_BLOCK_SILVER: Rect = Rect {
+    x: 56.0,
+    y: 24.0,
+    w: 8.0,
+    h: 8.0,
+};
+pub const SPRITE_BLOCK_GOLD: Rect = Rect {
+    x: 56.0,
+    y: 32.0,
+    w: 8.0,
+    h: 8.0,
+};
+pub const SPRITE_BLOCK_PLATINUM: Rect = Rect {
+    x: 56.0,
+    y: 40.0,
+    w: 8.0,
+    h: 8.0,
+};
+pub const SPRITE_BLOCK_URANIUM: Rect = Rect {
+    x: 56.0,
+    y: 48.0,
+    w: 8.0,
+    h: 8.0,
+};
+
+// gem blocks
+pub const SPRITE_BLOCK_DIAMOND: Rect = Rect {
+    x: 64.0,
+    y: 0.0,
+    w: 8.0,
+    h: 8.0,
+};
+pub const SPRITE_BLOCK_EMERALD: Rect = Rect {
+    x: 64.0,
+    y: 8.0,
+    w: 8.0,
+    h: 8.0,
+};
+pub const SPRITE_BLOCK_RUBY: Rect = Rect {
+    x: 64.0,
+    y: 16.0,
+    w: 8.0,
+    h: 8.0,
+};
+pub const SPRITE_BLOCK_SAPPHIRE: Rect = Rect {
+    x: 64.0,
+    y: 24.0,
+    w: 8.0,
+    h: 8.0,
+};
+
+// continued ore,gem blocks
+pub const SPRITE_BLOCK_AMETHYST: Rect = Rect {
+    x: 72.0,
+    y: 0.0,
+    w: 32.0,
+    h: 80.0,
 };
 
 // Animation
